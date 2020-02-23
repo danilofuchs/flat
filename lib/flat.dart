04ec7699090209ecb6b8b07f31e22fd690c9a726
Danilo Fuchs <danilo_fuchs@hotmail.com>
2020-02-22 21:43:31 -0300

    refactor: Simplify logic based on npm's flat

--- lib/flat.dart
+++ lib/flat.dart
@@ -3,44 +3,33 @@
 /// If no [delimiter] is specified, will separate depth levels by `.`
 /// If you don't want to flatten arrays (with 0, 1,... indexes), use [safe] mode
 Map<String, dynamic> flatten(
-  Map<String, dynamic> obj, {
+  Map<String, dynamic> target, {
   String delimiter = ".",
   bool safe = false,
 }) {
-  scopedFlatten(
-    Map<String, dynamic> obj, {
-    String newDelimiter,
-    bool newSafe,
-  }) {
-    return flatten(obj,
-        delimiter: newDelimiter ?? delimiter, safe: newSafe ?? safe);
+  Map<String, Object> result = {};
+
+  void step(Map<String, dynamic> obj, [String previousKey]) {
+    obj.forEach((key, value) {
+      final isMap = value is Map;
+      final isList = value is List;
+
+      final newKey = previousKey != null ? "$previousKey$delimiter$key" : key;
+
+      if (isMap) {
+        return step(value, newKey);
+      }
+      if (isList && !safe) {
+        return step(_listToMap(value), newKey);
+      }
+      result[newKey] = value;
+    });
   }
 
-  Map<String, Object> result = {};
-  obj.forEach((key, value) {
-    if (value is Map) {
-      result.addAll(scopedFlatten(value).map((deepKey, deepValue) =>
-          MapEntry("$key$delimiter$deepKey", deepValue)));
-      return;
-    }
-    if (value is List) {
-      if (safe) {
-        result[key] = value;
-      } else {
-        result.addAll(scopedFlatten(_listToMap(value)).map(
-            (deepKey, deepValue) =>
-                MapEntry("$key$delimiter$deepKey", deepValue)));
-      }
-      return;
-    }
-    result[key] = value;
-  });
+  step(target);
+
   return result;
 }
 
 Map<String, dynamic> _listToMap(List<dynamic> list) =>
     list.asMap().map((key, value) => MapEntry(key.toString(), value));
-
-main() {
-  flatten({"a": 1});
-}
